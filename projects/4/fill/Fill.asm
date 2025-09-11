0892d405eba81c4900f5496c08565d790e0bcad3
EliF <ejf012@bucknell.edu>
2025-09-11 10:22:02 -0400

    update project 4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -9,3 +9,56 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+//KBD - SCREEN = a 512*256 = 131072/16=8192 which is the amount of registers to write to the screen 24576 - 16384 = 8192
+
+(WAIT)
+@KBD
+D=M
+@FILL
+M=D;JGT
+//When KBD is valued jump to fill
+@WAIT
+0;JMP
+//While KBD=0 wait
+
+
+(HOLD)
+@KBD
+D=M
+@EMPTY
+M=D;JEQ
+//When KBD is empty jump to EMPTY
+
+//************************************compare if at end
+@HOLD
+M=D;JGT
+//While KBD=1 and at the end wait
+
+
+(FILL)
+//start the filling process
+@SCREEN
+//Fill a pixel (set to -1)
+
+//Move 16 bits down
+
+//Check if KBD is still valued AND we've reached the end
+//If still valued and we're still working: come back to FILL
+
+//If valued and we're at the end: jump to hold
+
+//If empty: start emptying pixels
+
+
+(EMPTY)
+//Start the emptying process
+
+//Empty a pixel
+
+//Move a pixel up
+
+//Check if KBD is still unvalued AND if we've reached the end
+//If still unvalued and we're still working: come back to EMPTY
+
+//If still unvalued and we're at the end: jump to WAIT
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+//Initialize R2 to zero
+@2
+M=0
+
+//Jump to out if R0 or R1 are 0
+@0
+D=M
+@END
+D;JEQ
+//
+@1
+D=M
+@END
+D;JEQ
+
+(LOOP)
+    //jump to OUT if R1<=0
+    @1
+    D=M
+    @END
+    D;JLE
+
+    //if not, loop
+    @ADD
+    0;JMP
+
+(ADD)
+    //Add one copy of R0 to register R2
+    @0
+    D=M
+    @2
+    M=D+M
+
+    //subtract one from R1
+    @1
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
